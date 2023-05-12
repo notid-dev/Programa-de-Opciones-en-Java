17609b1dcedf07c8833e2803981e9e45c2876c25
Heriberto Fernandez <hfernandezaguirre@gmail.com>
2023-05-11 19:05:21 -0500

    Agregue auriculares en las opciones

--- OptionDialog.java
+++ OptionDialog.java
@@ -3,7 +3,7 @@ import javax.swing.JOptionPane;
 public class OptionDialog {
     public static void main(String[] args) {
 
-        String[] arreglo = { "Monitores", "Teclados", "Pantallas", "Bocinas" };
+        String[] arreglo = { "Monitores", "Teclados", "Pantallas", "Bocinas", "Auriculares"};
 
         int opcion = JOptionPane.showOptionDialog(null, "Elige una palabra...", "Escoge una opción", 0,
                 JOptionPane.QUESTION_MESSAGE, null, arreglo, "Teclados");
